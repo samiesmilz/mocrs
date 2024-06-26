4edf978caa717394c3192935986fd46c9ed9ed9b
samiesmilz <samiesmilz@gmail.com>
2024-06-26 13:28:44 +0800

    set environment

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@
 /** Express app for mocrs. */
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Import routes and middleware
 import authRoutes from "./routes/auth.js";
@@ -15,11 +17,12 @@ import morgan from "morgan";
 
 // Set up the express app
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || LIVE_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -29,14 +32,30 @@ app.use(morgan("tiny"));
 app.use(authenticateJWT);
 app.use(rateLimiter);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "dist")));
+
 // Routes to handle requests
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/rooms", roomRoutes);
 
 /** Handle 404 errors -- this matches everything else */
+// app.use(function (req, res, next) {
+//   return next(new NotFoundError());
+// });
+
 app.use(function (req, res, next) {
-  return next(new NotFoundError());
+  if (req.path.startsWith("/api")) {
+    return next(new NotFoundError());
+  } else {
+    next();
+  }
+});
+
+// Serve the index.html for all other routes (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 /** Generic error handler; anything unhandled goes here. */
