20089f1799667c881d95e303e52047c29721e0bb
samiesmilz <samiesmilz@gmail.com>
2024-07-03 02:25:44 +0800

    live url

--- frontend/mocrs/src/services/api.js
+++ frontend/mocrs/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-// import.meta.env.VITE_API_URL;
+
+const LIVE_URL = import.meta.env.VITE_LIVE_URL;
 const API_URL = import.meta.env.VITE_API_URL;
 
 // Create an axios instance with default configurations
 const apiClient = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL || LIVE_URL,
   headers: {
     "Content-Type": "application/json",
   },
