bd0bf1c63bb16a4022cfbc0a8abd9c15f210de60
samiesmilz <samiesmilz@gmail.com>
2024-07-03 11:10:00 +0800

    changes in api.js

--- frontend/mocrs/src/services/api.js
+++ frontend/mocrs/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const LIVE_URL = import.meta.env.VITE_LIVE_URL;
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://mocrs-backend.onrender.com/api";
 
 // Create an axios instance with default configurations
 const apiClient = axios.create({
